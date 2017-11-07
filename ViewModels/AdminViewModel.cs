c6a0974a47002c511cd13fdf74b5a4d85aa948c9
toDev10 <33443457+toDev10@users.noreply.github.com>
2017-11-07 15:21:17 -0600

    Add files via upload

--- ViewModels/AdminViewModel.cs
+++ ViewModels/AdminViewModel.cs
@@ -1,1 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace BabyShop.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; }
+    }
+}
