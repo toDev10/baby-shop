8b62d4c863dda7247b59901412884240fe4e1d00
toDev10 <33443457+toDev10@users.noreply.github.com>
2017-11-07 14:55:31 -0600

    Add files via upload

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BabyShop.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult FileUploadLimitExceeded()
+        {
+            return View();
+        }
+    }
+}
